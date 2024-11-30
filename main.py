5fdae03575a774f2cb1e3e1b314546df3973ef9d
KrzysztofMastuszewski <krzysiek.matuszewski.pl@gmail.com>
2024-11-30 11:20:42 +0100

    struktura

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello woord")
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return render_template("index.html")
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
