4106f4f4e84d2071a30b79e5fcd0b60c0543cff6
KrzysztofMastuszewski <krzysiek.matuszewski.pl@gmail.com>
2024-11-30 13:01:42 +0100

    baza

---
+++ bit.session.sql
@@ -0,0 +1,38 @@
+-- @block
+CREATE TABLE users(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255)NOT NULL UNIQUE,
+    password VARCHAR(255),
+    is_admin BOOLEAN
+);
+
+-- @block
+CREATE TABLE questions(
+    question_id INT PRIMARY KEY AUTO_INCREMENT,
+    question VARCHAR(255) NOT NULL UNIQUE,
+    axis VARCHAR(1) NOT NULL
+);
+
+-- @block
+CREATE TABLE results(
+    result_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    axis_x INT NOT NULL,
+    axis_y INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- @block
+CREATE TABLE hobbys(
+    hobby_id INT PRIMARY KEY AUTO_INCREMENT,
+    hobby VARCHAR(255) NOT NULL UNIQUE,
+    category_id INT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+-- @block
+CREATE TABLE categories(
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category VARCHAR(255) NOT NULL UNIQUE
+);
